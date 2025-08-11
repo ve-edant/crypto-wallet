b7acba0dd6533dec3d9a888492347c8f416da4d4
ve-edant <vedantsalunke17473@gmail.com>
2025-08-11 15:43:12 +0530

    hero_updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,33 +6,39 @@ import { useRouter } from "next/navigation";
 
 export default function Homepage() {
   const router = useRouter();
+
   return (
-    <section className="flex items-center justify-center max-h-screen bg-[#fafafa] px-6 py-24">
-      <div className="max-w-7xl w-full grid grid-cols-1 lg:grid-cols-2 gap-10 items-center">
+    <section className="flex items-center justify-center min-h-screen bg-[#fafafa] px-4 sm:px-6 py-12 sm:py-24">
+      <div className="max-w-7xl w-full flex flex-col-reverse lg:flex-row items-center lg:items-center gap-10">
+        
         {/* Left Section */}
-        <div className="flex flex-col items-start justify-center w-full max-w-xl px-4 sm:px-0">
-          <h1 className="text-3xl sm:text-4xl lg:text-6xl font-semibold text-gray-900 leading-tight tracking-tight">
-            Get Easy Access to<br/>Your Crypto with<br/>
+        <div className="flex flex-col items-start justify-center w-full lg:w-1/2 max-w-xl">
+          <h1 className="text-2xl sm:text-4xl lg:text-6xl font-semibold text-gray-900 leading-tight tracking-tight">
+            Get Easy Access to
+            <br />
+            Your Crypto with
+            <br />
             <span className="text-emerald-500">Company Wallet</span>
           </h1>
 
-          <p className="mt-4 text-base sm:text-lg text-gray-600 leading-relaxed">
+          <p className="mt-3 sm:mt-4 text-sm sm:text-lg text-gray-600 leading-relaxed">
             Your all-in-one crypto wallet to send, receive, and swap digital
             assets—anytime, anywhere.
           </p>
 
           {/* Buttons */}
-          <div className="mt-6 flex flex-col sm:flex-row gap-4 w-full sm:w-auto">
-            <button onClick={() => router.push('/dashboard')} className="border border-gray-300 px-6 py-3 rounded-lg font-medium bg-emerald-300 hover:bg-emerald-500 cursor-pointer transition duration-200">
+          <div className="mt-5 sm:mt-6 flex flex-col sm:flex-row gap-3 sm:gap-4 w-full sm:w-auto">
+            <button
+              onClick={() => router.push("/dashboard")}
+              className="w-full sm:w-auto border border-gray-300 px-6 py-3 rounded-lg font-medium bg-emerald-300 hover:bg-emerald-500 cursor-pointer transition duration-200"
+            >
               Get Started
             </button>
           </div>
-
-      
         </div>
 
         {/* Right Section */}
-        <div className="relative w-full h-[500px]">
+        <div className="relative w-full lg:w-1/2 h-[400px] sm:h-[400px] lg:h-[500px] flex justify-center">
           <Image
             src={heroImg}
             alt="Orbix Wallet Preview"
