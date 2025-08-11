09543266ebb58684c32c3b5813d0b8977753235c
ve-edant <vedantsalunke17473@gmail.com>
2025-08-11 15:37:47 +0530

    hero_updated.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { useRouter } from "next/navigation";
 export default function Homepage() {
   const router = useRouter();
   return (
-    <section className="flex items-center justify-center min-h-screen bg-white px-6 py-24">
+    <section className="flex items-center justify-center max-h-screen bg-[#fafafa] px-6 py-24">
       <div className="max-w-7xl w-full grid grid-cols-1 lg:grid-cols-2 gap-10 items-center">
         {/* Left Section */}
         <div className="flex flex-col items-start justify-center w-full max-w-xl px-4 sm:px-0">
@@ -23,25 +23,12 @@ export default function Homepage() {
 
           {/* Buttons */}
           <div className="mt-6 flex flex-col sm:flex-row gap-4 w-full sm:w-auto">
-            <button className="bg-emerald-500 text-white px-6 py-3 rounded-lg font-medium shadow hover:bg-emerald-600 transition duration-200">
-              Download App Now
-            </button>
-            <button className="border border-gray-300 px-6 py-3 rounded-lg font-medium hover:bg-gray-100 transition duration-200">
-              Access From Web
+            <button onClick={() => router.push('/dashboard')} className="border border-gray-300 px-6 py-3 rounded-lg font-medium bg-emerald-300 hover:bg-emerald-500 cursor-pointer transition duration-200">
+              Get Started
             </button>
           </div>
 
-          {/* Ratings */}
-          <div className="mt-8 flex flex-col sm:flex-row gap-6 text-gray-800">
-            <div className="flex items-center gap-2">
-              <p className="text-2xl font-bold">4.7</p>
-              <span className="text-sm text-gray-500">App Store Ratings</span>
-            </div>
-            <div className="flex items-center gap-2">
-              <p className="text-2xl font-bold">4.8</p>
-              <span className="text-sm text-gray-500">Google Play Ratings</span>
-            </div>
-          </div>
+      
         </div>
 
         {/* Right Section */}
